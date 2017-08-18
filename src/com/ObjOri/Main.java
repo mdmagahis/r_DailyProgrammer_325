f902359489f3874a124fe1df3b4070ff6ab21527
Mark Magahis <mark.magahis@gmail.com>
2017-08-17 23:42:49 -0500

    First attempts at reading each line from input.txt. Goal is to write input into 2D array

--- src/com/ObjOri/Main.java
+++ src/com/ObjOri/Main.java
@@ -1,31 +1,56 @@
 package com.ObjOri;
 
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 
 public class Main {
 
     public static void main(String[] args) throws FileNotFoundException {
+        LineNumberReader numLines = new LineNumberReader(new FileReader("resources/input1.txt"));
+        char[][] matrix = new char[numLines.getLineNumber()][numLines.getLineNumber()];
+
         BufferedReader inputFile = new BufferedReader(new FileReader("resources/input1.txt"));
         String s;
 
+        int line = numLines.getLineNumber();
+        System.out.println(line);
+
         try {
             while ((s = inputFile.readLine()) != null) {
-                for (char c : s.toCharArray()) {
-                    // Read line to char
-                    System.out.print(c);
+                String[] tokens = s.split(" ");
+                int i = 0;
+
+                for (String c : tokens) {
+                    // Read line char-by-char, ignoring spaces
+
                 }
-                // Debug -- Separate by line
-                System.out.println();
+
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
 
+        /**
+         * Original code to read each line char-by-char
+         * Not so efficient
+         */
+//        try {
+//            while ((s = inputFile.readLine()) != null) {
+//
+//                for (char c : s.toCharArray()) {
+//                    // Read line char-by-char, ignoring spaces
+//                    if (c != ' ') {
+//                        System.out.print(c);
+//
+//                    }
+//                }
+//                // Debug -- Separate by line
+//                System.out.println();
+//            }
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
 
-//        char[][] matrix = new char[][];
+
 
     }
 }
