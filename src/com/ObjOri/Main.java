d67deb90b9789b79d8ba16eaea2efcd279f529fd
Mark Magahis <mark.magahis@gmail.com>
2017-08-17 22:34:20 -0500

    Code for reading input character-by-character.

--- src/com/ObjOri/Main.java
+++ src/com/ObjOri/Main.java
@@ -1,8 +1,31 @@
 package com.ObjOri;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+    public static void main(String[] args) throws FileNotFoundException {
+        BufferedReader inputFile = new BufferedReader(new FileReader("resources/input1.txt"));
+        String s;
+
+        try {
+            while ((s = inputFile.readLine()) != null) {
+                for (char c : s.toCharArray()) {
+                    // Read line to char
+                    System.out.print(c);
+                }
+                // Debug -- Separate by line
+                System.out.println();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+//        char[][] matrix = new char[][];
+
     }
 }
