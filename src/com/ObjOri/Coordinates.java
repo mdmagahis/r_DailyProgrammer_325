3e7f9eda1e53c91789684448990772c0dd69dda2
Mark Magahis <mark.magahis@gmail.com>
2017-08-20 17:48:53 -0500

    Adds java class Coordinates

---
+++ src/com/ObjOri/Coordinates.java
@@ -0,0 +1,19 @@
+package com.ObjOri;
+
+public class Coordinates {
+    private int row, col;
+
+    public Coordinates (int row, int col) {
+        this.row = row;
+        this.col = col;
+    }
+
+    int getRow() {
+        return row;
+    }
+
+    int getCol() {
+        return col;
+    }
+
+}
